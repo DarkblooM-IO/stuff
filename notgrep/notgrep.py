f9611592a11d1024d604925d79a6b24bb731a420
DarkblooM-IO <darkbloom.io@proton.me>
2025-08-28 01:10:16 +0200

    Thu Aug 28 01:10:16 AM CEST 2025

--- notgrep/notgrep.py
+++ notgrep/notgrep.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
-import argparse, re
+import argparse, re, sys
 
 parser = argparse.ArgumentParser(prog="grep")
 parser.add_argument("pattern", help="Regex to search for")
 parser.add_argument("file", help="File to search through")
 args = parser.parse_args()
 
-def main():
+def main() -> int:
+    file = None
+    try:
+        file = open(args.file, "r")
+    except FileNotFoundError:
+        print(f"couldn't find file {args.file}")
+        return 1
+
     pattern = re.compile(args.pattern)
-    with open(args.file, "r") as file:
-        for line in file:
-            if pattern.search(line):
-                print(line.strip())
+
+    for line in file:
+        if pattern.search(line):
+            print(line.strip())
+
+    file.close()
+
+    return 0
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
