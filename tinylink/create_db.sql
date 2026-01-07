461910147cd0e6e83b5b0ed61870a9ff5d801709
DarkblooM-IO <darkbloom.io@proton.me>
2026-01-07 10:44:26 +0100

    Wed Jan  7 10:44:26 AM CET 2026

--- tinylink/create_db.sql
+++ tinylink/create_db.sql
@@ -1,10 +1,13 @@
+-- create base db
 DROP DATABASE IF EXISTS tinylink;
 CREATE DATABASE tinylink;
 USE tinylink;
 
+-- initiate table for redirections
 CREATE TABLE redirects (
   id INT AUTO_INCREMENT PRIMARY KEY,
   destination VARCHAR(512) NOT NULL
 );
 
+-- create example entry
 INSERT INTO redirects (destination) VALUES ('https://example.com');
