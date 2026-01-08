06eda4a89f789d22fb8e9065aeae5c9bbd3494bd
DarkblooM-IO <darkbloom.io@proton.me>
2026-01-08 17:52:05 +0100

    Thu Jan  8 05:52:05 PM CET 2026

--- tinylink/src/index.php
+++ tinylink/src/index.php
@@ -1,7 +1,24 @@
+<?php
+define("ROUTE", $_SERVER["REQUEST_URI"]);
+include("tinylink.php");
+?>
 <!DOCTYPE html>
 <html>
   <head>
-    <style>* { font-family: sans-serif }</style>
+    <style>
+      * { font-family: sans-serif }
+    </style>
   </head>
-  <body><?php include("tinylink.php"); ?></body>
+  <body>
+    <?php
+    switch (ROUTE) {
+      case "":
+      case "/":
+        echo "homepage";
+        break;
+      default:
+        redirect($db);
+    }
+    ?>
+  </body>
 </html>
