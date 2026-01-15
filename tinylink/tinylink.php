1bbf5472dbc77c686138f93a3b1a7dffea1db8cd
DarkblooM-IO <darkbloom.io@proton.me>
2026-01-15 16:50:21 +0100

    Thu Jan 15 04:50:21 PM CET 2026

--- tinylink/tinylink.php
+++ tinylink/tinylink.php
@@ -2,17 +2,19 @@
 function redirect($db, $route) {
   $id = explode("/", $route)[1];
 
-  if (preg_match("/^\d+$/", $id)) {
-    $stmt = $db->prepare("SELECT destination FROM redirects WHERE id = :destID");
-    $stmt->bindParam(":destID", $id);
-    $stmt->execute();
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+  if (!preg_match("/^\d+$/", $id))
+    return;
 
-    if ($result) {
-      $destination = $result["destination"];
-      echo "<script>window.location.href = '$destination'</script>";
-    } else {
-      echo "tinylink: no entry";
-    }
+  $stmt = $db->prepare("SELECT destination FROM redirects WHERE id = :destID");
+  $stmt->bindParam(":destID", $id);
+  $stmt->execute();
+  $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  if (!$result) {
+    echo "tinylink: no entry";
+    return;
   }
+
+  $destination = $result["destination"];
+  echo "<script>window.location.href = '$destination'</script>";
 }
